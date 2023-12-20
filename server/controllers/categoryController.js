37aeb5275bde9681adc0032b113f7be49388644a
axai <02b3akshay@gmail.com>
2023-12-20 19:55:55 +0530

    Add Category api crud operations

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -31,4 +31,88 @@ const createCategoryController = async (req, res) => {
 	}
 };
 
-module.exports = { createCategoryController };
+const updateCategoryController = async (req, res) => {
+	try {
+		const { name } = req.body;
+		const { id } = req.params;
+		const category = await categoryModel.findByIdAndUpdate(
+			id,
+			{ name, slug: slugify(name) },
+			{ new: true },
+		);
+		res.status(200).send({
+			success: true,
+			message: 'Category updated successfully!',
+			category,
+		});
+	} catch (error) {
+		console.log(error);
+		res.send(500).send({
+			success: false,
+			message: 'Error in Updating Category',
+			error,
+		});
+	}
+};
+
+const categoryController = async (req, res) => {
+	try {
+		const category = await categoryModel.find({});
+		res.status(200).send({
+			success: true,
+			message: 'All categories list',
+			category,
+		});
+	} catch (error) {
+		console.log(error);
+		res.send(500).send({
+			success: false,
+			message: 'Error in fetching categories',
+			error,
+		});
+	}
+};
+
+const singleCategoryController = async (req, res) => {
+	try {
+		const category = await categoryModel.findOne({ slug: req.params.slug });
+		res.status(200).send({
+			success: true,
+			message: 'Fetching single category success!',
+			category,
+		});
+	} catch (error) {
+		console.log(error);
+		res.send(500).send({
+			success: false,
+			message: 'Error in fetching category',
+			error,
+		});
+	}
+};
+
+const deleteCategoryController = async (req, res) => {
+	try {
+		const { id } = req.params;
+		await categoryModel.findByIdAndDelete(id);
+		res.status(200).send({
+			success: true,
+			message: 'Category deleted successfully!',
+		});
+	} catch (error) {
+		console.log(error);
+		res.send(500).send({
+			success: false,
+			message: 'Error while deleting category',
+			error,
+		});
+	}
+};
+
+module.exports = {
+	createCategoryController,
+	updateCategoryController,
+	categoryController,
+	singleCategoryController,
+	deleteCategoryController,
+};
--- server/routes/categoryRoutes.js
+++ server/routes/categoryRoutes.js
@@ -2,15 +2,42 @@ const express = require('express');
 const { requireSignIn, isAdmin } = require('../middlewares/authMiddleware');
 const {
 	createCategoryController,
+	updateCategoryController,
+	categoryController,
+	singleCategoryController,
+	deleteCategoryController,
 } = require('../controllers/categoryController');
 const router = express.Router();
 
 // routes
+// create category
 router.post(
 	'/create-category',
 	requireSignIn,
 	isAdmin,
 	createCategoryController,
+);
+
+// update category
+router.put(
+	'/update-category/:id',
+	requireSignIn,
+	isAdmin,
+	updateCategoryController,
+);
+
+// getAll categories
+router.get('/get-categories', categoryController);
+
+// single category
+router.get('/get-category/:slug', singleCategoryController);
+
+// delete category
+router.delete(
+	'/delete-category/:id',
+	requireSignIn,
+	isAdmin,
+	deleteCategoryController,
 );
 
 module.exports = router;
